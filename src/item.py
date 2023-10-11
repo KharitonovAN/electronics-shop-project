48fa765792b9daea5dc98c22f7aea9ff76c56328
HAN <han82@mail.ru>
2023-10-11 23:13:29 +0300

    homework-2

--- src/item.py
+++ src/item.py
@@ -1,35 +1,71 @@
+import codecs
+import csv
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
+    """Класс для представления товара в магазине."""
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
+        """Создание экземпляра класса item.
         :param name: Название товара.
         :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
+        :param quantity: Количество товара в магазине."""
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        self.__name = name
 
         Item.all.append(self)
 
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
+        """Рассчитывает общую стоимость конкретного товара в магазине.
+        :return: Общая стоимость товара."""
         return self.price * self.quantity
 
+
     def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
+        """Применяет установленную скидку для конкретного товара."""
         self.price *= self.pay_rate
+
+
+    @property
+    def name(self):
+        """Добавляем геттер для `name`, используя @property"""
+        return self.__name
+
+
+    @name.setter
+    def name(self, name):
+        """Добавляем сеттер для `name`, используя @property для проверки длины наименования
+        товара не больше 10 симвовов. В противном случае, обрезать строку"""
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+
+    @classmethod
+    def instantiate_from_csv(cls, filename: str) -> None:
+        """Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_"""
+        cls.all.clear()
+        with codecs.open(filename, 'r', encoding='utf-8', errors='replace') as file:
+            data = csv.DictReader(file)
+            items = []
+            for d in data:
+                name = d['name']
+                price = float(d['price'])
+                quantity = int(d['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+
+            cls.all = items
+
+
+    @staticmethod
+    def string_to_number(str_number: str):
+        """Cтатический метод, возвращающий число из числа-строки"""
+        number = str_number.split('.')
+        return int(number[0])
--- tests/test_item.py
+++ tests/test_item.py
@@ -3,18 +3,28 @@ from src.item import Item
 
 
 def test_calculate_total_price():
-    """
-    Тестирование метода calculate_total_price класса Item.
-    """
+    """Тестирование метода calculate_total_price класса Item."""
     item = Item('Test', 5, 5)
     assert item.calculate_total_price() == 25
 
 
 def test_apply_discount():
-    """
-    Тестирование метода apply_discount класса Item.
-    """
-    item = Item('Test', 100, 1)
-    item.pay_rate = 0.9
+    """Тестирование метода apply_discount класса Item."""
+    item = Item('Test', 20.0, 5)
+    item.pay_rate = 0.5
     item.apply_discount()
-    assert item.price == 9.0
+    assert item.price == 10
+
+
+def test_name_setter():
+    """Проверка на изменение имени"""
+    item = Item('Test', 20.0, 5)
+    item.name = 'Test_1'
+    assert item.name == 'Test_1'
+
+
+def test_len_name():
+    """Проверка длины имени не более 10 символов"""
+    item = Item('Test', 20.0, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
