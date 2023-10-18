8e04de0cdb8f3ce161613c9b12e89680cdbf8310
HAN <han82@mail.ru>
2023-10-18 21:55:18 +0300

    homework-5

--- src/keyboard.py
+++ src/keyboard.py
@@ -7,8 +7,7 @@ class MixinLang:
         """Инициализация класс-миксина keyboard
         language: Язык раскладки
         По умолчанию 'EN'"""
-        language = 'EN'
-        self.language = language
+        self.language = 'EN'
 
     def change_lang(self):
         """Функция для изменения языка (раскладки клавиатуры).
@@ -20,14 +19,12 @@ class MixinLang:
 
     def save_lang(self):
         """Функция для хранения раскладки клавиатуры"""
-        print(f'Язык раскладки клавиатуры - {self.language}')
+        return self.language
 
 
 class Keyboard(Item, MixinLang):
     """Класс `Keyboard` для товара 'клавиатура'"""
-    def __init__(self, name: str, price: float, quantity: int, languange='EN'):
+    def __init__(self, name: str, price: float, quantity: int) -> None:
         """Инициализация класса `Keyboard` наследуемый от 'Item' и с 'подмешиванием' из 'MixinLang'"""
         super().__init__(name, price, quantity)
-        self.language = languange
-        if self.language.upper() != 'EN' and self.language.upper() != 'RU':
-            raise ValueError('Всего поддерживается два языка: `EN` и `RU`')
+        MixinLang.__init__(self)
